01e65307ef1bbb1b00db513edfe66fa8a902f2ff
初夏丶迟暮 <oncwnuL3sPkgb_OE4HJiQ6-Edagc@git.weixin.qq.com>
2019-05-27 17:50:42 +0800

    first commit

---
+++ src/components/NavBread.vue
@@ -0,0 +1,30 @@
+<!--  -->
+<template>
+      <div class="nav-breadcrumb-wrap">
+        <div class="container">
+          <nav class="nav-breadcrumb">
+            <a href="/">Home</a>
+             <slot name="one"></slot>
+          </nav>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+
+  methods: {}
+}
+
+</script>
+<style  scoped>
+</style>
